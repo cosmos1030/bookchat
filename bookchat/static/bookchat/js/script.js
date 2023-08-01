95aa5b15f2968558912798555df957d6878e821a
cosmos1030 <dyk6208@gmail.com>
2023-08-02 05:22:47 +0900

    웹소켓 wss로 변환

--- bookchat/static/bookchat/js/script.js
+++ bookchat/static/bookchat/js/script.js
@@ -112,7 +112,7 @@ function displayMessage(sender, message) {
 const bookId = getBookIdFromUrl()
 
 const chatSocket = new WebSocket(
-  'ws://'
+  'wss://'
   + window.location.host
   + '/ws/chat/'
   + bookId

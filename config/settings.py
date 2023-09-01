70517f8ec2ce5766eff78d05d5eed5c23be7eea6
cosmos1030 <dyk6208@gmail.com>
2023-09-01 19:35:02 +0900

    DB 연결 완료

--- config/settings.py
+++ config/settings.py
@@ -108,23 +108,23 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'HOST': 'svc.sel4.cloudtype.app',
-#         'PORT': '30915',
-#         'NAME': 'postgresql',
-#         'USER': 'root',
-#         'PASSWORD': env('PASSWORD'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'svc.sel4.cloudtype.app',
+        'PORT': '31154',
+        'NAME': 'bookchatting-db',
+        'USER': 'root',
+        'PASSWORD': env('PASSWORD'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

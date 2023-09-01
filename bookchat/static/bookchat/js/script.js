4d4e711ef9a0783827680f5efdb16dfa03daf202
cosmos1030 <dyk6208@gmail.com>
2023-09-01 19:36:56 +0900

    wss

--- bookchat/static/bookchat/js/script.js
+++ bookchat/static/bookchat/js/script.js
@@ -47,7 +47,7 @@ function displayMessage(sender, message) {
 const bookId = getBookIdFromUrl()
 
 const chatSocket = new WebSocket(
-  'ws://'
+  'wss://'
   + window.location.host
   + '/ws/chat/'
   + bookId
